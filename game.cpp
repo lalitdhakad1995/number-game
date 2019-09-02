438fef14758e1f7a26c3c90537ef8ae011ab9ec8
lalitdhakad1995 <47225487+lalitdhakad1995@users.noreply.github.com>
2019-09-02 23:06:30 +0530

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,13 @@
+// Iterative C++ program to reverse 
+// a linked list 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{ 
+char  c[]="k";
+char * p=NULL;
+cout<<*c;
+	return 0; 
+} 
+
